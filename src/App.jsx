4dab8e4eefcc53d997f8df4907ac7ffd90a097ba
ismail-dev-code <ismail.online0385678@gmail.com>
2025-04-10 09:45:14 +0600

    tailwind, daisyUI and navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       
-      <h1>My Auction Gallery</h1>
+      <Navbar></Navbar>
      
       
     </>
